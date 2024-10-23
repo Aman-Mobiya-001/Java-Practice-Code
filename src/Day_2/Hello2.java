17bb01f234d5182a1f0552afeab966c99c5571a9
Aman Mobiya <amanmobiya0012@email.com>
2024-10-23 21:20:44 +0530

    added

---
+++ src/Day_2/Hello2.java
@@ -0,0 +1,12 @@
+package Day_2;
+
+public class Hello2 {
+	private static void Sout() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello2 World!!");
+	}
+	
+	public static void main(String[] args) {
+		Sout();
+	}
+}
